b4d3edbe8dcfdd47410469339a5d8a025627a560
tatomr <34355847+tatomr@users.noreply.github.com>
2018-04-02 13:26:34 -0400

    Add files via upload

---
+++ Tutorial 6.6_South America/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Tutorial_6._6_South_America
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CountriesListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.getCountriesButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CountriesListBox
+            // 
+            this.CountriesListBox.FormattingEnabled = true;
+            this.CountriesListBox.Location = new System.Drawing.Point(55, 48);
+            this.CountriesListBox.Name = "CountriesListBox";
+            this.CountriesListBox.Size = new System.Drawing.Size(190, 147);
+            this.CountriesListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The Countries of South America";
+            // 
+            // getCountriesButton
+            // 
+            this.getCountriesButton.Location = new System.Drawing.Point(55, 201);
+            this.getCountriesButton.Name = "getCountriesButton";
+            this.getCountriesButton.Size = new System.Drawing.Size(75, 49);
+            this.getCountriesButton.TabIndex = 2;
+            this.getCountriesButton.Text = "Get Countries";
+            this.getCountriesButton.UseVisualStyleBackColor = true;
+            this.getCountriesButton.Click += new System.EventHandler(this.getCountriesButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(170, 201);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 49);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.getCountriesButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CountriesListBox);
+            this.Name = "Form1";
+            this.Text = "South America";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox CountriesListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button getCountriesButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Tutorial 6.6_South America/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Tutorial_6._6_South_America
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getCountriesButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declare a variable totoal to hold a country name.
+                string countryName;
+
+                //Declare a StreamReader Variable.
+                StreamReader inputFile;
+
+                //Open the file and get a streamReader object.
+                inputFile = File.OpenText("Countries.txt");
+
+                //Clear anything current in the Listbox.
+                CountriesListBox.Items.Clear();
+
+                //Read the file contents.
+                while (!inputFile.EndOfStream)
+                {
+                    //Get a country name.
+                    countryName = inputFile.ReadLine();
+
+                    //Add the country name to the ListBox.
+                    CountriesListBox.Items.Add(countryName);
+                }
+
+                //Close the file.
+                inputFile.Close();
+
+            }
+            catch (Exception ex)
+            {
+                //Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+            }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Close the form
+            this.Close();
+        }
+    }
+    }
